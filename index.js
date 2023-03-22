58fa8eacd2d7ff49ee1650a73f50d999fc92759c
Yinson Tso <yinson.tso@gmail.com>
2023-03-22 11:55:27 -0400

    spent too long on this

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+findMatching = (arr, str) => {
+    let ans = arr.filter(name => name.toLowerCase() === str.toLowerCase())
+    return ans
+}
+
+fuzzyMatch = (arr,str) => {
+    
+    let ans = arr.filter(name => name.slice(0,str.length) === str.slice())
+
+    return ans
+}
+
+matchName = (arr, str) => {
+
+    // arr.name === str
+    let ans = arr.filter(driver => driver.name === str)
+    return ans
+}
